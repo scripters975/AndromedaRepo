6c048e8c9bb5d93f0bcc97bd2e901ea6b071e28f
scripters975 <scripters.quote@gmail.com>
2022-10-22 22:53:46 +0530

    remove comanted code from db connection file

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -9,14 +9,6 @@ function databseConnection (){
     console.log(error);
 })
 }
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://koniat:<password>@koniatdb.ynfjwur.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 module.exports = {
     databseConnection
 }
